cfc9eafc1a1e045af1acd45a4a9ad07a7ac8dcad
Swapnil Kuwar <swapnilkuwar1995@gmail.com>
2019-11-07 20:47:01 +0530

    libs resourses fixed.

--- src/main/java/com/cloud/security/service/NotificationServiceImpl.java
+++ src/main/java/com/cloud/security/service/NotificationServiceImpl.java
@@ -1,13 +1,4 @@
 package com.cloud.security.service;
-import java.util.Properties;
-
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;  
 
@@ -15,26 +6,6 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class NotificationServiceImpl implements NotificationService{	 
 	public boolean sendMail(String to, String subject, String msg) {
-	   /*   String host = "localhost";
-	      String from = "admin@sudarshanwabale.info";
-	      Properties properties = System.getProperties();  
-	      properties.setProperty("mail.smtp.host", host);  
-	      Session session = Session.getDefaultInstance(properties);  
-	  
-	      try{  
-	         MimeMessage message = new MimeMessage(session);  
-	         message.setFrom(new InternetAddress(from));  
-	         message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));  
-	         message.setSubject(subject);  
-	         message.setText(msg);
-	  
-	         Transport.send(message);  
-	         return true;
-	      }catch (MessagingException mex) {
-	    	  mex.printStackTrace();
-	    	  return false;
-	      }
-	      */
 		return true;
 	}
 }	
